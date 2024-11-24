d49d540ef5f9d29dbe1747031deb74d1dc868613
kcspex <kcspex@gmail.com>
2024-11-24 12:27:38 +0100

    updated

--- phpscript/insert-studentsscoretbl.php
+++ phpscript/insert-studentsscoretbl.php
@@ -37,8 +37,8 @@ include ('../database/config.php');
             else
             {
                
-                $sqlInsert =("INSERT INTO `score` (`StudentID`, `ClassID`, `SectionID`, `SubjectID`, `Session`, `Term`) 
-                VALUES ('$studentid','$classid','$classsectionactual','$subjects','$session','$term')");
+                $sqlInsert = "INSERT INTO `score` (`StudentID`, `ClassID`, `SectionID`, `SubjectID`, `Session`, `Term`) 
+                VALUES ('$studentid','$classid','$classsectionactual','$subjects','$session','$term')";
 
                 if(mysqli_query($link, $sqlInsert))
                 {
