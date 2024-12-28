d57d6062f8ee1e7982f8c2aa446824ba9372205c
kcspex <kcspex@gmail.com>
2024-12-29 00:00:47 +0100

    updated

--- application/models/Student_model.php
+++ application/models/Student_model.php
@@ -610,9 +610,9 @@ class Student_model extends MY_Model
 
         $orderable_fields = 'students.admission_no,students.firstname,students.middlename,students.lastname,classes.class,students.father_name,students.dob,students.gender,categories.category,students.mobileno,';
 
-        if (!empty($field_name)) {
-            $orderable_fields .= ',' . $field_name . '12';
-        }
+        // if (!empty($field_name)) {
+        //     $orderable_fields .= ',' . $field_name . '12';
+        // }
 
         $this->datatables->select('classes.id AS `class_id`,students.id,student_session.id as student_session_id,classes.class,sections.id AS `section_id`,sections.section,students.id,students.admission_no , students.roll_no,students.admission_date,students.firstname,students.middlename,  students.lastname,students.image,    students.mobileno, students.email ,students.state ,   students.city , students.pincode ,     students.religion,     students.dob ,students.current_address,    students.permanent_address,IFNULL(students.category_id, 0) as `category_id`,IFNULL(categories.category, "") as `category`,      students.adhar_no,students.samagra_id,students.bank_account_no,students.bank_name, students.ifsc_code ,students.father_name , students.guardian_name , students.guardian_relation,students.guardian_phone,students.guardian_address,students.is_active ,students.created_at ,students.updated_at,students.gender,students.rte,student_session.session_id,' . $field_variable);
         $this->datatables->join('student_session', 'student_session.student_id = students.id');
@@ -1937,9 +1937,9 @@ class Student_model extends MY_Model
 
         $orderable_fields = 'students.admission_no,students.firstname,classes.class,students.father_name,students.dob,students.gender,categories.category,students.mobileno';
 
-        if (!empty($field_name)) {
-            $orderable_fields .= ',' . $field_name .'11';
-        }
+        // if (!empty($field_name)) {
+        //     $orderable_fields .= ',' . $field_name .'11';
+        // }
 
          $this->datatables
             ->select('classes.id AS `class_id`,student_session.id as student_session_id,students.id,classes.class,sections.id AS `section_id`,sections.section,students.id,students.admission_no , students.roll_no,students.admission_date,students.firstname,students.middlename,  students.lastname,students.image,    students.mobileno, students.email ,students.state ,   students.city , students.pincode ,     students.religion,     students.dob ,students.current_address,    students.permanent_address,IFNULL(students.category_id, 0) as `category_id`,IFNULL(categories.category, "") as `category`,students.adhar_no,students.samagra_id,students.bank_account_no,students.bank_name, students.ifsc_code , students.guardian_name , students.guardian_relation,students.guardian_phone,students.guardian_address,students.is_active ,students.created_at ,students.updated_at,students.father_name,students.app_key,students.parent_app_key,students.rte,students.gender,'. $field_variable)
