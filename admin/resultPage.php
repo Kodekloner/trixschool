945dc91f38abbec74e4ee1c6d8e8377969f3b1e5
Kodekloner <billgateokoye@gmail.com>
2025-02-11 15:15:22 +0100

    updated result page

--- admin/resultPage.php
+++ admin/resultPage.php
@@ -54,15 +54,15 @@ include('../database/config.php');
         /* Define the page size and margins for printing */
         @page {
             size: A4 portrait; /* Change to landscape if needed */
-            margin: 10mm;      /* Adjust as necessary */
+            margin: 5mm;      /* Adjust as necessary */
         }
         
         /* Scale the printable container down to ensure it fits on one page */
         #printable {
             /* Adjust the scale value as needed based on your content size */
-            transform: scale(0.80);
-            transform-origin: top left;
-            width: 1200px;
+            /* transform: scale(0.80);
+            transform-origin: top left; */
+            width: 1000px;
         }
         
         /* Prevent page breaks inside key containers */
