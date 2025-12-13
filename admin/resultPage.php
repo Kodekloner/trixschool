24ccd729da74a4b7406b973ce381834a985e7fb7
Kodekloner <billgateokoye@gmail.com>
2025-12-13 11:40:00 +0100

    purplins result adjusting

--- admin/resultPage.php
+++ admin/resultPage.php
@@ -2075,6 +2075,8 @@ $studsection = $rowGetsections['section'];
 
                                 $gettotgrade = floatval(round($rowgettotalgrade['average'] / $row_cntgetsubscore, 2));
 
+                                echo $gettotgrade;
+
                                 $gettotscore = floatval(round($rowgettotalgrade['average'], 2));
 
                                 $sqlgetClasscount = ("SELECT DISTINCT(StudentID) FROM `score` WHERE (`exam` !='0' OR `ca1` !='0' OR `ca2` !='0' OR `ca3` !='0' OR `ca4` !='0' OR `ca5` !='0' OR `ca6` !='0' OR `ca7` !='0' OR `ca8` !='0' OR `ca9` !='0' OR `ca10` !='0') AND ClassID = '$classid' AND SubjectID IN (
